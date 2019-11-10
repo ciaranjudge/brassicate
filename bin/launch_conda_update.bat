e13f367bb9ba72b8edc700c18f5f8ff51c0cf5d7
Ciaran Judge <CiaranJudge@welfare.irlgov.ie>
2019-11-10 22:29:35 +0000

    Changed target for script

--- bin/launch_conda_update.bat
+++ bin/launch_conda_update.bat
@@ -3,5 +3,5 @@ echo --------------------------------------------------------
 echo                  *** DON'T PANIC! ***
 echo --------------------------------------------------------
 call %LOCALAPPDATA%\Continuum\miniconda3\Scripts\activate.bat
-call python .\brassicate\conda\update_conda.py
+call python .\brassicate\brassicate.py
 
